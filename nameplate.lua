3763c61b1da022e1fee3a8f3030dc31541a1e7f6
Vardex <12969608+Vardex@users.noreply.github.com>
2022-10-30 14:50:51 +0100

    Fix nameplate scaling

--- nameplate.lua
+++ nameplate.lua
@@ -2,17 +2,25 @@ local addonName, addonTable = ...
 
 addonTable.nameplateCVars = {
     nameplateMotion = 1,
-    --nameplateOverlapH = 0.8
-    nameplateOverlapV = 0.8,
+    nameplateOverlapV = 1.5,
+    nameplateGlobalScale = 1,
+    nameplateMinScale = 1,
+    nameplateMaxScale = 1,
+    nameplateLargerScale = 1,
+    nameplateSelectedScale = 1
 }
 
 function addonTable.nameplateStyle(self)
-    self:SetSize(160, 30)
+    local width, height = 200, 30
+    self:SetSize(width, height)
     self:SetPoint("CENTER")
+    local scale = UIParent:GetEffectiveScale()
+    C_NamePlate.SetNamePlateEnemySize(math.floor(width * scale), math.floor(height * scale))
+    self:SetScale(scale)
     addonTable.elementsFactory.makeBorder(self, true)
     addonTable.elementsFactory.makeHealthBar(self)
-    addonTable.elementsFactory.makeNameText(self, "LEFT", 5, 0, 8)
-    addonTable.elementsFactory.makeHealthFullText(self, "RIGHT", -5, 0, 8)
+    addonTable.elementsFactory.makeNameText(self, "LEFT", 5, 0)
+    addonTable.elementsFactory.makeHealthFullText(self, "RIGHT", -5, 0)
     addonTable.elementsFactory.makeCastBar(self)
     addonTable.elementsFactory.makeAuras(self, true)
 end
